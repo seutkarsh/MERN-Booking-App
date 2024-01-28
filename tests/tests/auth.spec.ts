8107c99366fdaaec1dafc1dc75e970a853ca0ee8
Utkarsh Sharma <seutkarsh@gmail.com>
2024-01-29 00:22:21 +0530

    Test Automation Done
    Signed-off-by: Utkarsh Sharma <seutkarsh@gmail.com>

--- tests/tests/auth.spec.ts
+++ tests/tests/auth.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 const UI_URL = "http://localhost:3000/"
+const testRegisterEmail: string = `testemail${Math.floor(Math.random()*90000)+10000}@test.com`
 
 test('should allow the user to sign in', async ({ page }) => {
   await page.goto(UI_URL)
@@ -24,3 +25,25 @@ test('should allow the user to sign in', async ({ page }) => {
   await expect(page.getByRole("link",{name:"My Hotels"})).toBeVisible();
 });
 
+
+test("should allow user to register", async({page})=>{
+  await page.goto(UI_URL)
+
+  //get to register page using sign in then register link
+  await page.getByRole("link",{name:"Sign In"}).click()
+  await page.getByRole("link",{name:" Create an account here"}).click()
+  await expect(page.getByRole("heading",{name:"Create an Account"})).toBeVisible();
+
+  //add data to fields
+  await page.locator("[name=firstName]").fill("TestFirstName")
+  await page.locator("[name=lastName]").fill("TestLastName")
+  await page.locator("[name=email]").fill(testRegisterEmail)
+  await page.locator("[name=password]").fill("password")
+  await page.locator("[name=confirmPassword]").fill("password")
+
+  //click create account button
+  await page.getByRole("button",{name:"Create Account"}).click()
+
+  //assert registration successful
+  await expect(page.getByText("Registration Successful")).toBeVisible();
+})
