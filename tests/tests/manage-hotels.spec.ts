e6835769a889685202a9c9d6f613d667b3701eb4
Utkarsh Sharma <seutkarsh@gmail.com>
2024-02-13 09:54:56 +0530

    Edit Form Test Case Done
    Signed-off-by: Utkarsh Sharma <seutkarsh@gmail.com>

--- tests/tests/manage-hotels.spec.ts
+++ tests/tests/manage-hotels.spec.ts
@@ -59,3 +59,16 @@ test("should display hotels" , async ({page})=>{
 
     await expect(page.getByRole("link",{name:"View Details"})).toBeVisible()
 })
+
+test("should edit hotel" , async ({page})=>{
+    await page.goto(`${UI_URL}my-hotels`);
+
+    await page.getByRole("link",{name:"View Details"}).click()
+    await page.waitForSelector('[name="name"]',{state:"attached"})
+    await expect(page.locator('[name="name"]')).toHaveValue('Test Hotel')
+    await page.locator('[name="name"]').fill("Test Hotel Updated")
+    await page.getByRole("button",{name:"Save"}).click();
+    await expect(page.getByText("Hotel Updated Successfully")).toBeVisible({timeout:30000})
+
+
+})
