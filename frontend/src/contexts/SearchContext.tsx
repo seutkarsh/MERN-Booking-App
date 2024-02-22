022ce83ed91f5b55f356928662ffdd70076ae556
Utkarsh Sharma <seutkarsh@gmail.com>
2024-02-22 14:12:04 +0530

    Storing search parameters to session storage to prevent loss at refresh
    Signed-off-by: Utkarsh Sharma <seutkarsh@gmail.com>

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -24,11 +24,27 @@ export const SearchContextProvider = ({
 }: {
     children: React.ReactElement
 }): React.ReactElement => {
-    const [destination, setDestination] = useState<string>('')
-    const [checkIn, setCheckIn] = useState<Date>(new Date())
-    const [checkOut, setCheckOut] = useState<Date>(new Date())
-    const [adultCount, setAdultCount] = useState<number>(1)
-    const [childCount, setChildCount] = useState<number>(0)
+    const [destination, setDestination] = useState<string>(
+        () => sessionStorage.getItem('destination') || ''
+    )
+    const [checkIn, setCheckIn] = useState<Date>(
+        () =>
+            new Date(
+                sessionStorage.getItem('checkIn') || new Date().toISOString()
+            )
+    )
+    const [checkOut, setCheckOut] = useState<Date>(
+        () =>
+            new Date(
+                sessionStorage.getItem('checkOut') || new Date().toISOString()
+            )
+    )
+    const [adultCount, setAdultCount] = useState<number>(() =>
+        parseInt(sessionStorage.getItem('adultCount') || '1')
+    )
+    const [childCount, setChildCount] = useState<number>(() =>
+        parseInt(sessionStorage.getItem('childCount') || '0')
+    )
     const [hotelId, setHotelId] = useState<string>('')
 
     const saveSearchValues = (
@@ -45,7 +61,14 @@ export const SearchContextProvider = ({
         setChildCount(childCount)
         if (hotelId) {
             setHotelId(hotelId)
+            sessionStorage.setItem('hotelId', hotelId)
         }
+
+        sessionStorage.setItem('destination', destination)
+        sessionStorage.setItem('checkIn', checkIn.toISOString())
+        sessionStorage.setItem('checkOut', checkOut.toISOString())
+        sessionStorage.setItem('adultCount', adultCount.toString())
+        sessionStorage.setItem('childCount', childCount.toString())
     }
     return (
         <SearchContext.Provider
